e3b21fa29a568a40ced51a8b3b0439883a34628c
Khandu Shinde <khandushinde2018@gmail.com>
2026-08-22 13:03:11 +0530

    removing few more

--- modules/virtual-network/outputs.tf
+++ modules/virtual-network/outputs.tf
@@ -1,24 +1,24 @@
-output "vnet_id" {
-  description = "ID of the VNet"
-  value       = azurerm_virtual_network.main.id
-}
+# output "vnet_id" {
+#   description = "ID of the VNet"
+#   value       = azurerm_virtual_network.main.id
+# }
 
-output "vnet_name" {
-  description = "Name of the VNet"
-  value       = azurerm_virtual_network.main.name
-}
+# output "vnet_name" {
+#   description = "Name of the VNet"
+#   value       = azurerm_virtual_network.main.name
+# }
 
-output "public_subnet_id" {
-  description = "ID of the public/ingress subnet"
-  value       = azurerm_subnet.public.id
-}
+# output "public_subnet_id" {
+#   description = "ID of the public/ingress subnet"
+#   value       = azurerm_subnet.public.id
+# }
 
-output "private_subnet_id" {
-  description = "ID of the private (AKS) subnet"
-  value       = azurerm_subnet.private.id
-}
+# output "private_subnet_id" {
+#   description = "ID of the private (AKS) subnet"
+#   value       = azurerm_subnet.private.id
+# }
 
-output "database_subnet_id" {
-  description = "ID of the delegated database subnet"
-  value       = azurerm_subnet.database.id
-}
+# output "database_subnet_id" {
+#   description = "ID of the delegated database subnet"
+#   value       = azurerm_subnet.database.id
+# }
