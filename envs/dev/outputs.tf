1daf3ba2197a6e4ba99908444a5753dd794c970f
Khandu Shinde <khandushinde2018@gmail.com>
2026-08-22 12:50:50 +0530

    test

--- envs/dev/outputs.tf
+++ envs/dev/outputs.tf
@@ -1,9 +1,9 @@
-output "postgres_fqdn" {
-  description = "PostgreSQL Flexible Server FQDN"
-  value       = module.postgresql.server_fqdn
-}
+# output "postgres_fqdn" {
+#   description = "PostgreSQL Flexible Server FQDN"
+#   value       = module.postgresql.server_fqdn
+# }
 
-output "aks_cluster_name" {
-  description = "AKS cluster name"
-  value       = module.aks.cluster_name
-}
+# output "aks_cluster_name" {
+#   description = "AKS cluster name"
+#   value       = module.aks.cluster_name
+# }
